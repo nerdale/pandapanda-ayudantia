5d04d61abff7f39df001c19e7086e9203d2a2072
nerdale <alexandra.neira@gmail.com>
2017-05-29 18:09:45 -0400

    completing code

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,4 @@
+//Funcionalidad para incono X delete
 document.getElementsByClassName('fa')[0].addEventListener('click', function(){
 	var panda = document.getElementsByClassName('pandas')[0];
 	panda.classList.add('fantasma'); //agrego clase fantasma (display: none) a mi foto indicada
@@ -16,9 +17,53 @@ document.getElementsByClassName('fa')[3].addEventListener('click', function(){
 })
 
 //boton restaurar todas las fotos
-document.getElementById('restaurar').addEventListener('click', function(){
+document.getElementById('boton-restaurar').addEventListener('click', function(){
 	document.getElementsByClassName('pandas')[0].classList.remove('fantasma');
 	document.getElementsByClassName('pandas')[1].classList.remove('fantasma');
 	document.getElementsByClassName('pandas')[2].classList.remove('fantasma');
 	document.getElementsByClassName('pandas')[3].classList.remove('fantasma');
+})
+
+
+//Funcionalidad botón origin
+
+document.getElementById('boton-origen').addEventListener('click', function(){
+	var parrafoIzquierdo = document.getElementsByClassName('parrafo')[0];
+	parrafoIzquierdo.classList.toggle('fantasma');
+})
+
+/* LO MISMO QUE ARRIBA
+var botonOrigen = document.getElementById('boton-origen');
+botonOrigen.addEventListener('click', parrafoIzquierdo);
+function parrafoIzquierdo(){
+	var parrafoIzquierdo = document.getElementsByClassName('parrafo')[0];
+	parrafoIzquierdo.classList.toggle('fantasma');
+}
+*/
+
+//funcionalidad botoón extinción
+document.getElementById('boton-extincion').addEventListener('click', function(){
+	var parrafoIzquierdo = document.getElementsByClassName('parrafo')[1];
+	parrafoIzquierdo.classList.toggle('fantasma');
+})
+
+/* LO MISMO QUE ARRIBA
+var botonExtincion = document.getElementById('boton-extincion');
+botonExtincion.addEventListener('click', parrafoDerecho);
+function parrafoDerecho(){
+	var parrafoDerecho = document.getElementsByClassName('parrafo')[1];
+	parrafoDerecho.classList.toggle('fantasma');
+}
+*/
+
+//arreglo de fotos que agrego a mi html como contenido dinámico
+var images = ['assets/img/a1.jpg','assets/img/a2.jpg', 'assets/img/a3.jpg', 'assets/img/a4.jpg'];
+
+var contenedor = document.getElementsByClassName('nuevasFotos')[0];
+
+images.forEach(function(elemento){
+	var imagenesPanda = document.createElement('img');
+	imagenesPanda.setAttribute('src', elemento);
+	imagenesPanda.classList.add('nuevos-panda');
+	contenedor.appendChild(imagenesPanda);
 })
